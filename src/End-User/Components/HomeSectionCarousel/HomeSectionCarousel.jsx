21d881c02a530c63f6e1d592d839650f86dadfcb
Abdul Muheeth <abdulmuheeth88@gmail.com>
2023-12-01 11:14:33 +0530

    ...

--- src/End-User/Components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/End-User/Components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,43 +1,50 @@
 import React, { useState } from 'react'
 import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+// import 'react-alice-carousel/lib/alice-carousel.css';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import {Button} from "@mui/material"
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { mens_kurta } from '../../../Data/Men/men_kurta';
 
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({data, sectionName}) => {
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
         1024: { items: 5 },
     };
 
-    const [activeIndex, setActiveIndex] = useState(0)
+    const [activeIndex, setActiveIndex] = useState(0);
 
-    const slidePrev = () => setActiveIndex(activeIndex-1)
-    const slideNext = () => setActiveIndex(activeIndex+1)
+    const slidePrev = () => {
+      setActiveIndex((activeIndex) => activeIndex - 1)
+      console.log(activeIndex)
+    }
+    const slideNext = () => {
+      console.log("Clickedddddddddd")
+      setActiveIndex((activeIndex) => activeIndex + 1)
+      console.log(activeIndex)
+    } 
     const syncActiveIndex = ({item}) => setActiveIndex(item)
 
-    const items = [1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map((item) => <HomeSectionCard/>)
+    const items = data.slice(0,10).map((item) => <HomeSectionCard product={item}/>)
   return (
     <div className='border'>
+      <h2 className='text-2xl font-extrabold text-gray-800 py-5'>{sectionName}</h2>
         <div className='relative border'>
-            <AliceCarousel
-            infinite
-            disableDotsControls
-            disableButtonsControls
-            items={items}
-            responsive={responsive}
-            onSlideChanged={syncActiveIndex}
-            activeIndex={activeIndex}
-            />
-
-
-        { activeIndex !== items.length-5 && <Button onClick={slideNext} className='z-50' variant='contained' sx={{position:"absolute", top: "8rem", right:"0rem", transform: "translateX(50%) rotate(90deg)", bgcolor:"white"}} aria-label='next'> <KeyboardArrowUpIcon sx={{color:"black"}}/> </Button>}
-        { activeIndex !== items.length-5 && <Button onClick={slidePrev} className='z-50' variant='contained' sx={{position:"absolute", top: "8rem", left:"0rem", transform: "translateX(-50%) rotate(90deg)",  bgcolor:"white"}} aria-label='next'> <KeyboardArrowDownIcon sx={{color:"black"}}/> </Button>}
+        <AliceCarousel
+          disableButtonsControls
+          disableDotsControls
+          mouseTracking
+          items={items}
+          activeIndex={activeIndex}
+          responsive={responsive}
+          onSlideChanged={syncActiveIndex}
+          animationType="fadeout"
+          animationDuration={2000}
+        />
+        { <Button onClick={slidePrev} className='z-50' variant='contained' sx={{position:"absolute", top: "8rem", left:"0rem", transform: "translateX(-50%) rotate(90deg)",  bgcolor:"white"}} aria-label='next'> <KeyboardArrowDownIcon sx={{color:"black"}}/> </Button>}
+        { <Button onClick={slideNext} className='z-50' variant='contained' sx={{position:"absolute", top: "8rem", right:"0rem", transform: "translateX(50%) rotate(90deg)", bgcolor:"white"}} aria-label='next'> <KeyboardArrowUpIcon sx={{color:"black"}}/> </Button>}
         </div>
     </div>
   )
--- src/End-User/Pages/HomePage/HomePage.jsx
+++ src/End-User/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import MainCarousel from '../../Components/MainCarousel/MainCarousel'
 import HomeSectionCarousel from '../../Components/HomeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/Men/men_kurta'
 
 const HomePage = () => {
   return (
     <div>
         <MainCarousel />
         <div className='py-20 space-y-10 px-5 lg:px-10' >
-            <HomeSectionCarousel/>
+            <HomeSectionCarousel sectionName="Mens Kurta" data={mens_kurta}/>
+            <HomeSectionCarousel sectionName="Mens Shoes" data={mens_kurta}/>
+            <HomeSectionCarousel sectionName="Mens Shirt" data={mens_kurta}/>
+            <HomeSectionCarousel sectionName="Womens Saree" data={mens_kurta}/>
+            <HomeSectionCarousel sectionName="Womens Dress" data={mens_kurta}/>
         </div>
     </div>
   )
